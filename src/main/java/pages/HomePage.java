e354c5164d88c3726be5c43a64f562a714cb9a65
iamyosh <yoshedirisinghe28@gmail.com>
2025-12-16 23:35:44 +0530

    driver passed and wait added

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,4 +1,22 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class HomePage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    public MakeAppointmentPage clickAppointmentButton(){
+
+    }
 }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,7 +1,26 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
+
 public class BaseTest {
 
+    protected WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeClass
+    public void setDriver(){
+        driver = new ChromeDriver();
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
+
+        homePage = new HomePage(driver);
+
+
+        //click make appointment button
+
+    }
 
 
 }
