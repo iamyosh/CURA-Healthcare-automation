c36b043e9e12fe8fed9b7c285abaf3808a8e1608
iamyosh <yoshedirisinghe28@gmail.com>
2025-12-30 00:35:46 +0530

    screenshot taken

---
+++ src/test/java/tests/MakeAppointmentTest.java
@@ -0,0 +1,51 @@
+package tests;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.AppointmentLoginPage;
+import pages.MakeAppointmentPage;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class MakeAppointmentTest extends BaseTest{
+
+    //hv to add the login details again bcz each test is independent
+    @BeforeMethod
+    public void loginTest(){
+        AppointmentLoginPage loginPage = new AppointmentLoginPage(driver);
+        loginPage.setUserDetails("John Doe", "ThisIsNotAPassword");
+        loginPage.clickLoginButton();
+    }
+
+
+    @Test
+    public void selectOptions(){
+        MakeAppointmentPage makeAppointmentPage = new MakeAppointmentPage(driver);
+        makeAppointmentPage.selectFromDropdown("Seoul CURA Healthcare Center");
+
+        makeAppointmentPage.selectDate("27");
+
+        makeAppointmentPage.addComment("No comment");
+        makeAppointmentPage.clickAppointmentButton();
+    }
+
+    @AfterMethod
+    public void takeScreenshot(){
+        var camera = (TakesScreenshot) driver;
+        File screenshot = camera.getScreenshotAs(OutputType.FILE);
+        System.out.println("Screenshot of booked appointment taken in path: " + screenshot.getAbsolutePath());
+
+        try {
+            Files.move(screenshot.toPath(), new File("resources/screenshots/appointment.png").toPath());
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+    }
+}
